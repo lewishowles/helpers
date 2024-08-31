726a59799f8ae6e9bb72ac3d922ad4d8ab039d6c
Lewis Howles <lewishowles@users.noreply.github.com>
2024-08-31 16:14:09 +0100

    Neaten up imports with aliases

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			"@array": fileURLToPath(new URL("./lib/array/array", import.meta.url)),
+			"@general": fileURLToPath(new URL("./lib/general/general", import.meta.url)),
+			"@number": fileURLToPath(new URL("./lib/number/number", import.meta.url)),
+			"@object": fileURLToPath(new URL("./lib/object/object", import.meta.url)),
+			"@string": fileURLToPath(new URL("./lib/string/string", import.meta.url)),
+			"@vue": fileURLToPath(new URL("./lib/vue/vue", import.meta.url)),
+			"@chart": fileURLToPath(new URL("./lib/chart/chart", import.meta.url)),
+		},
+	},
 	build: {
 		lib: {
 			entry: getFiles([
