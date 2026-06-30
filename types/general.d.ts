2ba6181093a050bec1f0ede948b5c271833c1e6a
Lewis Howles <lewishowles@users.noreply.github.com>
2026-06-30 20:09:17 +0100

    feat(general): add debounce and throttle helpers

--- types/general.d.ts
+++ types/general.d.ts
@@ -10,6 +10,19 @@ type SettleOutcome<T = any> = {
 	results: SettleResult<T>[];
 };
 
+type TimingOptions = { leading?: boolean; trailing?: boolean };
+
+type ControlledFunction<T extends (...args: any[]) => any> = {
+	(...args: Parameters<T>): ReturnType<T> | undefined;
+	cancel(): void;
+	flush(): ReturnType<T> | undefined;
+};
+
+export declare function debounce<T extends (...args: any[]) => any>(
+	fn: T,
+	delay?: number,
+	options?: TimingOptions,
+): ControlledFunction<T>;
 export declare function getFriendlyDisplay(variable: any): string;
 export declare function isFunction(variable: any): variable is Function;
 export declare function resolveOrFallback<T, F>(
@@ -18,6 +31,11 @@ export declare function resolveOrFallback<T, F>(
 ): Promise<T | F>;
 export declare function settle<T = any>(promises: any[]): Promise<SettleOutcome<T>>;
 export declare function size(variable: any): number;
+export declare function throttle<T extends (...args: any[]) => any>(
+	fn: T,
+	delay?: number,
+	options?: TimingOptions,
+): ControlledFunction<T>;
 export declare function validateOrFallback<T>(
 	value: any,
 	comparison: ComparisonKeyword | ((value: any) => boolean),
